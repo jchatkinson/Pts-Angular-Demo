ccbfcccf6fa81a3292f0eabe83c243d6046922d1
Jeremy Atkinson <jchatkinson@gmail.com>
2019-01-09 09:38:23 -0800

    init commit, implementing Pts demo 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CanvasSpace, Pt, Group, CanvasForm } from 'pts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pts-demo';
+  space: CanvasSpace;
+  form: CanvasForm;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.space = new CanvasSpace('#demo');
+    this.space.setup({bgcolor: '#d3d3d3', retina: true, resize: true });
+    this.form = this.space.getForm();
+    this.space.add( () => this.form.point( this.space.pointer, 10));
+    this.space.bindMouse().bindTouch().play();
+  }
+
 }
